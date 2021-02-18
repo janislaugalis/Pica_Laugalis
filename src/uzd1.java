6ccb493f06830af70c8a2f529da5b66155fd2b6a
Jānis Laugalis <laugalis3211@gmail.com>
2021-02-18 23:06:12 +0200

    pievienot izveidot metodi

--- src/uzd1.java
+++ src/uzd1.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class uzd1 {
-	
 	public static int[][] izveidot(int[][] masivs){
 		Random rand = new Random();
 		for(int i=0; i<masivs.length; i++) {
@@ -20,104 +19,6 @@ public class uzd1 {
 		return masivs;
 	}
 	
-	public static void izvadit(int[][] masivs) {
-		String masivsVirkne="";
-		for(int i=0; i<masivs.length; i++) {
-			for(int j=0; j<masivs[i].length; j++) {
-				masivsVirkne = masivsVirkne+masivs[i][j]+" \t";
-			}
-			masivsVirkne = masivsVirkne+"\n";
-		}
-		JOptionPane.showMessageDialog(null, masivsVirkne);
-	}
-	
-	static void flip(int arr[], int i){
-        int temp, start = 0;
-        while (start < i)
-        {
-            temp = arr[start];
-            arr[start] = arr[i];
-            arr[i] = temp;
-            start++;
-            i--;
-        }
-    }
-	
-    static int findMax(int arr[], int n){
-        int mi, i;
-        for (mi = 0, i = 0; i < n; ++i)
-            if (arr[i] > arr[mi])
-                mi = i;
-        return mi;
-    }
-    
-    static int[][] pancakeSort(int masivs[][]){
-    	int n = masivs.length*masivs[0].length;
-    	int skaititajs=0;
-	    int[] arr = new int [masivs.length*masivs[0].length];
-	    for(int c=0; c<masivs.length; c++){
-			for(int b=0; b<masivs[c].length; b++){
-				arr[skaititajs] = masivs[c][b];
-				skaititajs++;
-			}
-		}
-	    
-        for (int curr_size = n; curr_size > 1; 
-                                 --curr_size)
-        {
-            int mi = findMax(arr, curr_size);
-            if (mi != curr_size-1)
-            {
-                flip(arr, mi);
-                flip(arr, curr_size-1);
-            }
-        }
-        skaititajs=0;
-		for(int c=0; c<masivs.length; c++){
-			for(int b=0; b<masivs[c].length; b++){
-				masivs[c][b] = arr[skaititajs];
-				skaititajs++;
-			}
-		}
-		JOptionPane.showMessageDialog(null, "Masīvs sakārtots augoši!");
-		return masivs;
-    }
-	
-	public static void saglabat(int[][] masivs) {
-		try {
-			FileWriter fw = new FileWriter("Laugalis.txt");
-			PrintWriter raksta = new PrintWriter(fw);
-			for(int i=0; i<masivs.length; i++) {
-				for(int j=0; j<masivs[i].length; j++) {
-					raksta.print(masivs[i][j]+" \t");
-				}
-				raksta.println();
-			}
-			JOptionPane.showMessageDialog(null, "Ierakstīts failā - `Laugalis.txt`");
-			raksta.close();
-		}catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "Kļūme ierakstot failā!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
-		}
-	}
-	
-	public static void nolasit(int[][] masivs) {
-	        try{
-	        	String masivsVirkne="";
-				FileReader fails = new FileReader("Laugalis.txt");
-	            Scanner myReader = new Scanner(fails);
-	            while (myReader.hasNextLine()) {
-	              String data = myReader.nextLine();
-	              masivsVirkne=masivsVirkne+data+"\n";
-	            }
-	            myReader.close();
-	            JOptionPane.showMessageDialog(null, masivsVirkne);
-	        }
-	        catch (IOException errorMessage){
-	            JOptionPane.showMessageDialog(null, "Failu nevar atrast!");
-	            System.out.println(errorMessage.getMessage());
-	            System.out.printf("Failu nevar atrast.\n\n");
-	        }
-	}
 
 	public static void main(String[] args) {
 		int izvele;
