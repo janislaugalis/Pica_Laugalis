02b53db60a59d2c948b260973c26e42c41beadc9
Jānis Laugalis <laugalis3211@gmail.com>
2021-02-26 00:13:31 +0200

    Izveido darbību izvēlnes paneli

--- src/Picerija_Laugalis.java
+++ src/Picerija_Laugalis.java
@@ -1,8 +1,9 @@
-import javax.swing.JOptionPane;
+
 
 public class Picerija_Laugalis {
 
 	public static void main(String[] args) {
+		int izvele;
 		double cena=0;
 		String vards="", uzvards="", epasts="", adrese="";
 		int talrunis=0;
@@ -10,20 +11,6 @@ public class Picerija_Laugalis {
 		String picasPiedevas="";
 		String picasMercites="";
 		String picasSanemsana="";
-		
-		 do {
-			 izvele = Integer.parseInt(JOptionPane.showInputDialog("1-Izveidot jaunu sūtijumu\n2-Izvadīt visus pasūtījumus\n0-Apturēt"));
-			 switch(izvele) {
-			 case 1:
-			 break;
-			 case 0:
-			             JOptionPane.showMessageDialog(null, "Programma apturēta!");
-			         break;
-			         default:
-			             JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
-			         break;
-			 }
-			         }while(izvele!=0);
 
 	}
 
