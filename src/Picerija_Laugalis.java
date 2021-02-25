a12e9c153a36893a6af0b27810f4213837ff4896
Jānis Laugalis <laugalis3211@gmail.com>
2021-02-26 00:44:35 +0200

    tiek piefiksēts picas lielums

--- src/Picerija_Laugalis.java
+++ src/Picerija_Laugalis.java
@@ -16,8 +16,14 @@ public class Picerija_Laugalis {
 			 izvele = Integer.parseInt(JOptionPane.showInputDialog("1-Izveidot jaunu sūtijumu\n2-Izvadīt visus pasūtījumus\n0-Apturēt"));
 			 switch(izvele) {
 			 case 1:
-			 break;
-			 case 0:
+				 cena=0;
+		            vards = JOptionPane.showInputDialog("Ievadiet savu vārdu: ");
+		            uzvards = JOptionPane.showInputDialog("Ievadiet savu uzvārdu: ");
+		            talrunis = Integer.parseInt(JOptionPane.showInputDialog("Ievadiet savu talruņa numuru: "));
+		            epasts = JOptionPane.showInputDialog("Ievadiet savu epastu: ");
+		            adrese = JOptionPane.showInputDialog("Ievadiet savu adresi (pilsēta, iela, māja, dzīvoklis): ");
+		            
+			
 			             JOptionPane.showMessageDialog(null, "Programma apturēta!");
 			         break;
 			         default:
