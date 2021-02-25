06424d4e0cc4d142f380fcfdefb9f9ced85f0685
Jānis Laugalis <laugalis3211@gmail.com>
2021-02-25 23:41:38 +0200

    Mainigo nodefinēšana

---
+++ src/Picerija_Laugalis.java
@@ -0,0 +1,16 @@
+
+public class Picerija_Laugalis {
+
+	public static void main(String[] args) {
+		int izvele;
+		double cena=0;
+		String vards="", uzvards="", epasts="", adrese="";
+		int talrunis=0;
+		String picasLielums="";
+		String picasPiedevas="";
+		String picasMercites="";
+		String picasSanemsana="";
+
+	}
+
+}
