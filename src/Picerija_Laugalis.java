2983b522f7769aa1331582a8336811365042f3f9
Jānis Laugalis <laugalis3211@gmail.com>
2021-02-25 23:52:42 +0200

    Izveido darbības izvēlnes paneli.

--- src/Picerija_Laugalis.java
+++ src/Picerija_Laugalis.java
@@ -2,7 +2,6 @@
 public class Picerija_Laugalis {
 
 	public static void main(String[] args) {
-		int izvele;
 		double cena=0;
 		String vards="", uzvards="", epasts="", adrese="";
 		int talrunis=0;
