99ae7097787fdf4eeca046dfad47a4fe0b648523
Jānis Laugalis <laugalis3211@gmail.com>
2021-02-26 00:21:50 +0200

    tiek piefiksēti klienta dati

--- src/Picerija_Laugalis.java
+++ src/Picerija_Laugalis.java
@@ -1,4 +1,4 @@
-
+import javax.swing.JOptionPane;
 
 public class Picerija_Laugalis {
 
@@ -11,6 +11,20 @@ public class Picerija_Laugalis {
 		String picasPiedevas="";
 		String picasMercites="";
 		String picasSanemsana="";
+		
+		 do {
+			 izvele = Integer.parseInt(JOptionPane.showInputDialog("1-Izveidot jaunu sūtijumu\n2-Izvadīt visus pasūtījumus\n0-Apturēt"));
+			 switch(izvele) {
+			 case 1:
+			 break;
+			 case 0:
+			             JOptionPane.showMessageDialog(null, "Programma apturēta!");
+			         break;
+			         default:
+			             JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
+			         break;
+			 }
+			         }while(izvele!=0);
 
 	}
 
